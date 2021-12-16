cabb9674eb4673c062e892d36968a9723d6ba3a0
Johirul Islam Nishat <jahirulislamn@gmail.com>
2021-12-16 23:47:23 +0600

    user dashboard update

--- src/Components/DashboardArea/UserArea/DashboardUser.js
+++ src/Components/DashboardArea/UserArea/DashboardUser.js
@@ -1,27 +1,64 @@
 import { Grid } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const DashboardUser = () => {
+    const appointmentBanner = {
 
+        backgroundColor: '#2095fc07',
+        backgroundBlendMode: 'darken',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+    }
+
+    // total services
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => setServices(data.length))
+    }, [])
+
+    // total bookings
+    const [bookings, setBookings] = useState([]);
+    const email = localStorage.getItem('email');
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/bookedService/${email}`)
+            .then(res => res.json())
+            .then(data => setBookings(data.length))
+    }, [])
+
+    // total engineers
+    const [engineers, setEngineers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/engineers')
+            .then(res => res.json())
+            .then(data => setEngineers(data.length))
+    }, [])
 
     return (
-        <div>
-            {/* <Box sx={{ width: '100%' }}>
+        <>
+            <Box sx={{ width: '100%' }}>
                 <Grid container spacing={{ xs: 2, md: 4 }} columns={{ xs: 4, sm: 8, md: 12 }}>
 
+                    {/* total services  */}
                     <Grid item xs={1} sm={2} md={3}>
                         <div style={{ height: 100, width: '100%', backgroundColor: '#f1536e', display: 'flex' }} className='rounded shadow-lg'>
                             <div style={{ textAlign: 'center', display: 'flex', fontSize: 26, color: 'white', justifyContent: 'center', width: '100%', alignItems: 'center' }} className='mx-auto '>
 
                                 <Grid item xs={6}>
                                     <div>
-                                        {users}
+                                        {services}
                                     </div>
                                 </Grid>
                                 <Grid item xs={6}>
                                     <div style={{ textAlign: 'left' }}>
-                                        Total Patient
+                                        Total Services
                                     </div>
                                 </Grid>
                             </div>
@@ -29,18 +66,19 @@ const DashboardUser = () => {
                         </div>
                     </Grid>
 
+                    {/* total booking */}
                     <Grid item xs={1} sm={2} md={3}>
                         <div style={{ height: 100, width: '100%', backgroundColor: '#3da5f4', display: 'flex', padding: 4 }} className='rounded shadow-lg'>
                             <div style={{ textAlign: 'center', display: 'flex', fontSize: 26, color: 'white', justifyContent: 'center', width: '100%', alignItems: 'center' }} className='mx-auto '>
 
                                 <Grid item xs={6}>
                                     <div>
-                                        0
+                                        {bookings}
                                     </div>
                                 </Grid>
                                 <Grid item xs={6}>
                                     <div style={{ textAlign: 'left' }}>
-                                        Pending Appointments
+                                        My Bookings
                                     </div>
                                 </Grid>
                             </div>
@@ -48,18 +86,19 @@ const DashboardUser = () => {
                         </div>
                     </Grid>
 
+                    {/* total engineers */}
                     <Grid item xs={1} sm={2} md={3}>
                         <div style={{ height: 100, width: '100%', backgroundColor: '#00c689', display: 'flex', padding: 4 }} className='rounded shadow-lg'>
                             <div style={{ textAlign: 'center', display: 'flex', fontSize: 26, color: 'white', justifyContent: 'center', width: '100%', alignItems: 'center' }} className='mx-auto '>
 
                                 <Grid item xs={6}>
                                     <div>
-                                        0
+                                        {engineers}
                                     </div>
                                 </Grid>
                                 <Grid item xs={6}>
                                     <div style={{ textAlign: 'left' }}>
-                                        Today's Appointments
+                                        Total Engineers
                                     </div>
                                 </Grid>
                             </div>
@@ -67,18 +106,19 @@ const DashboardUser = () => {
                         </div>
                     </Grid>
 
+                    {/* total teams */}
                     <Grid item xs={1} sm={2} md={3}>
                         <div style={{ height: 100, width: '100%', backgroundColor: '#fda006', display: 'flex', padding: 4 }} className='rounded shadow-lg'>
                             <div style={{ textAlign: 'center', display: 'flex', fontSize: 26, color: 'white', justifyContent: 'center', width: '100%', alignItems: 'center' }} className='mx-auto '>
 
                                 <Grid item xs={6}>
                                     <div>
-                                        {appointment}
+                                        {engineers}
                                     </div>
                                 </Grid>
                                 <Grid item xs={6}>
                                     <div style={{ textAlign: 'left' }}>
-                                        Total Appointments
+                                        Total Teams
                                     </div>
                                 </Grid>
                             </div>
@@ -87,9 +127,41 @@ const DashboardUser = () => {
                     </Grid>
                 </Grid>
 
+                <div className='mt-5'>
+                    <div>
+                        <Box style={appointmentBanner} sx={{ flexGrow: 1, paddingTop: 5, paddingBottom: 5 }}>
+                            <Container>
+                                <Grid container spacing={2}>
 
-            </Box> */}
-        </div>
+                                    <Grid item xs={12} md={7} sx={{ display: "flex", justifyContent: "center" }}>
+                                        <img style={{ width: '100%', padding: 15, }} src='https://i.ibb.co/ZKLS40f/slide1.png' alt="" />
+                                    </Grid>
+
+                                    <Grid item xs={12} md={5} sx={{ display: "flex", justifyContent: "flex-start", alignItems: 'center', textAlign: "left", paddingY: 4 }}>
+                                        <Box>
+                                            <Typography className='text-primary' variant="h5">
+                                                We are proving all type of cleaning solutions for every small and big businesses.    </Typography>
+
+                                            <Typography paragraph className='text-main' sx={{ marginTop: 2 }} >
+                                                Booking professional picture hangers through Handy gives you complete control over when and where they turn up. It is a long established fact that a reader will be distracted by the readable
+                                                content of a page when looking at its
+                                            </Typography>
+
+
+                                            <Link to='/services' uppercase='true'>
+                                                <button className=" btn-main py-1 px-3 mt-5">Get Our Service</button>
+                                            </Link>
+                                        </Box>
+                                    </Grid>
+
+                                </Grid>
+                            </Container>
+                        </Box>
+                    </div>
+                </div>
+
+            </Box>
+        </>
     );
 };
 
